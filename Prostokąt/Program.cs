c4302792fa0ce38c0e84c28928ebaf29874acc78
mac <mac@DESKTOP-LU3IG6O>
2020-11-28 18:06:30 +0100

    Add project files.

---
+++ Prostokąt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Prostokąt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj szerokość: ");
+            string xs = Console.ReadLine();
+            Console.Write("Podaj wysokość: ");
+            string ys = Console.ReadLine();
+
+            int x = int.Parse(xs);
+            int y = int.Parse(ys);
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+           
+            Console.ReadKey();
+        }
+    }
+}
